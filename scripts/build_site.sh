1d2a0e614b870b40d4d3e220ac6244a485577946
jamy.shih <jamy.shih@prophetstor.com>
2018-12-20 12:08:02 +0800

    update website static

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -16,3 +16,6 @@ mv dark_theme* static/css
 uglifyjs src/js/misc.js src/js/utils.js src/js/prism.js --mangle --compress -o static/js/all.min.js --source-map
 uglifyjs src/js/styleSwitcher.js --mangle --compress -o static/js/styleSwitcher.min.js --source-map
 svgstore -o static/img/icons.svg src/icons/**/*.svg
+
+tar -zcvf js.tar.gz src/js
+tar -zcvf css.tar.gz src/css
